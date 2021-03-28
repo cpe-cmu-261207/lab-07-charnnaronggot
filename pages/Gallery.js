557ade5291b149b35194c8b086ac75bb393ede6f
charnnaronggot <charnnarong_2542@hotmail.com>
2021-03-28 16:24:55 +0700

    -05

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -1,84 +1,82 @@
-import Link from 'next/link'
+import Link from "next/link";
 const Gallery = () => {
-    return (
+  return (
     <>
-            <table>
-                <table id="Head">
-                    <tr>
+      <table>
+        <table id="Head">
+          <tr>
+            <Link href="/"> Homepage</Link>
 
-                            <Link href="/"> Homepage</Link>
+            <Link href="/Gallery"> Gallery</Link>
 
-                            <Link href="/Gallery"> Gallery</Link>
-
-                            <Link href="/Contract">Contract </Link>
-                    </tr>
-                </table>
-                <table>
-                    <table>
-                        <tr>
-                            <td>
-                                <h1>Charnnarong Charroensanongkun</h1>
-                                <h3>Student</h3>
-                                <h2>Charnnarong_c@cmu.ac.th</h2>
-                            </td>
-                            <th>
-                                <img width="200px" src="Resource/พี่โต.jpg" alt="5"/>
-            </th>
+            <Link href="/Contract">Contract </Link>
           </tr>
-  </table>
-                    </table>
-                    <h1>Gallery</h1>
+        </table>
+        <table>
+          <table>
+            <tr>
+              <td>
+                <h1>Charnnarong Charroensanongkun</h1>
+                <h3>Student</h3>
+                <h2>Charnnarong_c@cmu.ac.th</h2>
+              </td>
+              <th>
+                <img width="200px" src="Resource/9.jpg" alt="5" />
+              </th>
+            </tr>
+          </table>
+        </table>
+        <h1>Gallery</h1>
 
-                    <table>
-                        <table border="1px">
-                            <tr>
-                                <th>
-                                    <img width="300px" src="Resource/1.png" alt="1" />
-                                    <p>Add a description here</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/2.jpg" alt="2" />
-                                    <p>Add a description here</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/3.jpg" alt="3" />
-                                    <p>Add a description here</p>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th>
-                                    <img width="300px" src="Resource/4.jpg" alt="4" />
-                                    <p>Add a description here</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/5.jpg" alt="5" />
-                                    <p>( ͡° ͜ʖ ͡°)</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/6.jpg" alt="6" />
-                                    <p>Add a description here</p>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th>
-                                    <img width="300px" src="Resource/7.jpg" alt="7" />
-                                    <p>Add a description here</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/8.jpg" alt="8" />
-                                    <p>Add a description here</p>
-                                </th>
-                                <th>
-                                    <img width="300px" src="Resource/9.jpg" alt="9" />
-                                    <p>Add a description here</p>
-                                </th>
-                            </tr>
-                        </table>
-                        </table>
-                        </table>
+        <table>
+          <table border="1px">
+            <tr>
+              <th>
+                <img width="300px" src="Resource/1.png" alt="1" />
+                <p>Stalin</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/2.jpg" alt="2" />
+                <p>Bertram Ramsay</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/3.jpg" alt="3" />
+                <p>Dog</p>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <img width="300px" src="Resource/4.png" alt="4" />
+                <p>cat</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/5.jpg" alt="5" />
+                <p>Hard bass</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/6.jpg" alt="6" />
+                <p>lana del ray</p>
+              </th>
+            </tr>
+            <tr>
+              <th>
+                <img width="300px" src="Resource/7.jpg" alt="7" />
+                <p>Redremasted</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/8.jpg" alt="8" />
+                <p>Stonk</p>
+              </th>
+              <th>
+                <img width="300px" src="Resource/9.jpg" alt="9" />
+                <p>Pee To</p>
+              </th>
+            </tr>
+          </table>
+        </table>
+      </table>
     </>
+  );
+};
 
-  )
-}
-
-export default Gallery
+export default Gallery;
--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,54 @@
-import { useEffect } from 'react';
-import axios from 'axios'
-import Link from 'next/link'
-const baseURL = 'https://dummyapi.io/data/api'
+import { useEffect } from "react";
+import axios from "axios";
+import Link from "next/link";
+const baseURL = "https://dummyapi.io/data/api";
 const Home = () => {
   return (
     <>
       <table>
         <table id="Head">
           <tr>
-
-              <Link href="/">Homepage</Link>
-
-              <Link href="/Gpacalc">GPA Calculator</Link>
+            <th>
+              <Link href="/">Homepage </Link>
+            </th>
+            <th>
+              <Link href="/Gpacalc">GPACalculator</Link>
+            </th>
+            <th>
               <Link href="/Gallery">Gallery</Link>
-
+            </th>
+            <th>
               <Link href="/Contract">Contract</Link>
+            </th>
+            <th>
               <Link href="/post">Posts</Link>
-
+            </th>
           </tr>
-          
+          <tr></tr>
         </table>
         <table>
           <table>
             <tr>
               <td>
-                <h1>Charnnarong Charoensaningkun  </h1>
+                <h1>Charnnarong Charoensaningkun </h1>
                 <h3>Student</h3>
                 <h2>Charnnarong_c@cmu.ac.th</h2>
               </td>
               <th>
-                <img width="200px" src="imgsrc/5.png" alt="5" />
+                <img width="200px" src="Resource/9.jpg" alt="5" />
               </th>
             </tr>
           </table>
         </table>
       </table>
       <h1>Profile</h1>
-      <p>นายชาญณรงค์ เจริญสนองกุล  620612146 </p>
+      <p>นายชาญณรงค์ เจริญสนองกุล 620612146 </p>
       <h3>Education</h3>
       <p>Sanpatong Wittayakom , Chiang Mai University</p>
       <h2>Skill&Technical</h2>
-      <p>C++   Java</p>
-
-      
+      <p>C++ Java</p>
     </>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
